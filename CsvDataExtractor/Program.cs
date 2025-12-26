3b4b1c1edc48590bead4a194f624845adb35e90f
Victor Hugo <vh.hugoon@hotmail.com>
2025-12-26 16:05:19 -0300

    Adiciona análises de dados: faturamento total e média

--- CsvDataExtractor/Program.cs
+++ CsvDataExtractor/Program.cs
@@ -1,12 +1,49 @@
-﻿using System;
+﻿using CsvDataExtractor;
+using System;
+using System.Collections.Generic;
+using System.IO;
 
-namespace MyApp
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        string caminho = "Data/vendas.csv";
+        string[] linhas = File.ReadAllLines(caminho);
+
+        List<Venda> vendas = new List<Venda>();
+
+        for (int i = 1; i < linhas.Length; i++) // pula o cabeçalho
         {
-            Console.WriteLine("Projeto CSV iniciado com sucesso");
+            string[] colunas = linhas[i].Split(',');
+
+            string produto = colunas[0];
+            int quantidade = int.Parse(colunas[1]);
+            decimal preco = decimal.Parse(colunas[2]);
+
+            Venda venda = new Venda(produto, quantidade, preco);
+            vendas.Add(venda);
+        }
+
+        foreach (var venda in vendas)
+        {
+            decimal total = venda.CalcularTotal();
+            Console.WriteLine($"Produto: {venda.Produto} | Total: {total}");
+        }
+
+        decimal faturamentoTotal = 0;
+
+
+        foreach (var venda in vendas)
+        {
+            faturamentoTotal += venda.CalcularTotal();
+        }
+        Console.WriteLine($"Faturamento total: {faturamentoTotal}");
+
+        if (vendas.Count > 0)
+        {
+            decimal media = faturamentoTotal / vendas.Count;
+            Console.WriteLine($"Média por venda: {media}");
         }
     }
 }
+
---
+++ CsvDataExtractor/Venda.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace CsvDataExtractor
+{
+    internal class Venda
+    {
+        public string Produto { get; private set; }
+        public int Quantidade { get; private set; }
+        public decimal Preco { get; private set; }
+
+
+        public Venda(string produto, int quantidade, decimal preco)
+        {
+
+            if (string.IsNullOrWhiteSpace(produto))
+                throw new Exception("Produto inválido");
+
+            if (quantidade <= 0)
+                throw new Exception("Quantidade inválida");
+
+            if (preco <= 0)
+                throw new Exception("Preço inválido");
+
+            Produto = produto;
+            Quantidade = quantidade;
+            Preco = preco;
+        }
+
+        public decimal CalcularTotal()
+        {
+            return Quantidade * Preco;
+        }
+
+    }
+}
