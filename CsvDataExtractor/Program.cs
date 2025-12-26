f254b48bf502341eac475901d290fbca3038098a
Victor Hugo <vh.hugoon@hotmail.com>
2025-12-26 11:24:09 -0300

    Projeto inicial: estrutura console em C#

---
+++ CsvDataExtractor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Projeto CSV iniciado com sucesso");
+        }
+    }
+}
